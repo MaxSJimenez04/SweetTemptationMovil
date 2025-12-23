658f3a51ac93d1556e48c9293e6113f6f4aecbc7
Max Jimenez <maximiliano.soto.j@gmail.com>
2025-12-22 18:59:10 -0600

    feat: agregar menu principal y ventana pedido

---
+++ app/src/main/java/com/example/sweettemptation/PedidoViewModel.java
@@ -0,0 +1,7 @@
+package com.example.sweettemptation;
+
+import androidx.lifecycle.ViewModel;
+
+public class PedidoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/sweettemptation/pedido.java
@@ -0,0 +1,36 @@
+package com.example.sweettemptation;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class pedido extends Fragment {
+
+    private PedidoViewModel mViewModel;
+
+    public static pedido newInstance() {
+        return new pedido();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pedido, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PedidoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
