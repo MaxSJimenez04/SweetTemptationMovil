799d142224377cebb47c2741e8bceaab571cbbba
Max Jimenez <maximiliano.soto.j@gmail.com>
2025-12-28 11:12:00 -0600

    feat: crear ticket con gRPC

--- app/src/main/java/com/example/sweettemptation/utils/Constantes.java
+++ app/src/main/java/com/example/sweettemptation/utils/Constantes.java
@@ -1,8 +1,8 @@
 package com.example.sweettemptation.utils;
 
 public class Constantes {
-    private static final String PUERTO = "8080";
-    private static final String PUERTO_GRPC = "9090";
+    public static final String PUERTO = "8080";
+    public static final int PUERTO_GRPC = 9090;
     private static final String IP = "localhost";
     public static final String URL = "http://" + IP + ":" + PUERTO + "/";
     public static final String URL_GRPC= "http://" + IP + ":" + PUERTO;
